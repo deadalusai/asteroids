266f470d3e58fa992797959259dd2c8d8cda92c7
Ben Fox <deadalus.ai@gmail.com>
2022-09-10 14:44:15 +1200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
+}
+
+pub struct HelloPlugin;
+
+fn add_people(mut commands: Commands) {
+    commands.spawn().insert(Person).insert(Name("Elaina Proctor".to_string()));
+    commands.spawn().insert(Person).insert(Name("Renzo Hume".to_string()));
+    commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
+}
+
+struct GreetTimer(Timer);
+
+impl GreetTimer {
+    fn test_timer_just_finished(&mut self, time: &Time) -> bool {
+        self.0.tick(time.delta()).just_finished()
+    }
+}
+
+fn greet_people(
+    time: Res<Time>,
+    mut timer: ResMut<GreetTimer>,
+    query: Query<&Name, With<Person>>
+) {
+    if timer.test_timer_just_finished(&time) {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
+    }
+}
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)));
+        app.add_startup_system(add_people);
+        app.add_system(greet_people);
+    }
+}
